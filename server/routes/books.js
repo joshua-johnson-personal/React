b093a791bc7851c8244857e03a5f74c94423dc4a
Joshua Johnson <joshua.johnson.personal@gmail.com>
2024-03-22 17:38:12 -0700

    Connecting the DB

---
+++ server/routes/books.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mysql = require('mysql');
+const router = express.Router();
+const connectionPool = require('../database/connection-pool')
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+
+    const book = {
+        'author': 'Charles Dickens',
+        'title': 'Great Expectations',
+        'published': '1861-01-01'
+    };
+
+    connectionPool.getPool().query('insert into books set ?', book, (err, result) => {
+        if(err) throw err;
+
+        console.log(result);
+    });
+
+  res.send('books here_blahbity_blah!');
+});
+
+module.exports = router;
